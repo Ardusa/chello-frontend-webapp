7f928917f4f1c975986fc8af4dd40b63b50757d7
Ardusa <ardusa05@gmail.com>
2025-02-18 20:47:58 -0500

    can now rearrange tasks using buttons

--- src/components/ProjectDisplay.jsx
+++ src/components/ProjectDisplay.jsx
@@ -5,7 +5,7 @@ import { fetchProjectDetails, createTask, updateTask, TaskCreate, fetchAccounts,
 import { SimpleTreeView, TreeItem2 } from "@mui/x-tree-view";
 import { CircularProgress, Typography, Button, Dialog, DialogTitle, DialogContent, TextField, DialogActions } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faPlus, faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import RotateRightIcon from '@mui/icons-material/RotateRight';
 import SettingsIcon from '@mui/icons-material/Settings';
@@ -57,7 +57,6 @@ const ProjectTaskTree = () => {
   const { project_id } = useParams();
   const navigate = useNavigate();
   const [project, setProject] = useState({});
-
   const [error, setError] = useState(null);
   const [open, setOpen] = useState(false);
   const [newTask, setNewTask] = useState(new TaskCreate({ project_id }));
@@ -65,48 +64,23 @@ const ProjectTaskTree = () => {
   const [taskDetails] = useState({});
   const [subtasksDict] = useState({});
   const [loading, setLoading] = useState(true);
-
   const [editingTask, setEditingTask] = useState(false);
   const [openTaskRef, setOpenTaskRef] = useState(null);
-
   const [renderedTasks, setRenderedTasks] = useState([]);
   const [expandedItems, setExpandedItems] = useState([]);
+  const [rootTaskOrder, setRootTaskOrder] = useState(0);
 
   useEffect(() => {
     const loadData = async () => {
       setLoading(true);
       await fetchAccountDetails();
       await loadProjectDetails();
-
       setExpandedItems(Object.keys(taskDetails));
-
       setLoading(false);
     };
 
     loadData();
   }, [project_id]);
-
-  const loadProjectDetails = async () => {
-    try {
-      const projectData = await fetchProjectDetails(project_id);
-      setProject({ ...projectData });
-
-      await fetchTaskDetailsRecursively(projectData.tasks);
-
-
-      const tasks = Object.keys(projectData.tasks).map((subtaskId) => {
-        const tree = renderTree(subtaskId, projectData.tasks);
-        // console.log("tree", tree);
-        return tree;
-      });
-
-      setRenderedTasks(tasks);
-
-    } catch (error) {
-      console.error("Error fetching project details:", error);
-      setError("Error fetching project details.");
-    }
-  };
 
   const fetchAccountDetails = async () => {
     try {
@@ -122,9 +96,32 @@ const ProjectTaskTree = () => {
     }
   };
 
+  const loadProjectDetails = async () => {
+    try {
+      const projectData = await fetchProjectDetails(project_id);
+      setProject({ ...projectData });
+
+      await fetchTaskDetailsRecursively(projectData.tasks);
+
+      setRootTaskOrder(Object.keys(projectData.tasks).length);
+
+      const tasks = Object.keys(projectData.tasks).map((subtaskId) => {
+        const tree = renderTree(subtaskId, projectData.tasks);
+        return tree;
+      });
+
+      setRenderedTasks(tasks);
+    } catch (error) {
+      console.error("Error fetching project details:", error);
+      setError("Error fetching project details.");
+    }
+  };
+
   const fetchTaskDetailsRecursively = async (tasks) => {
     try {
-      for (const [taskId, subTasks] of Object.entries(tasks)) {
+      const taskIds = Object.keys(tasks).sort((a, b) => tasks[a].order - tasks[b].order);
+      for (const taskId of taskIds) {
+        const subTasks = tasks[taskId];
         const task = await fetchTaskDetails(taskId);
         if (!task) {
           throw new Error("Failed to fetch task details for task:", taskId);
@@ -137,13 +134,11 @@ const ProjectTaskTree = () => {
           }
 
           for (const [parentId, subtasks] of Object.entries(dict)) {
-            // if the parentId is the parent of the task or the subtasks has the task already, assign it
             if (parentId === task.parent_task_id || subtasks[task.id]) {
               dict[parentId][task.id] = {};
               return true;
             }
 
-            // if the subtasks is empty, skip to the next set of keys and values
             if (Object.keys(subtasks).length === 0) {
               continue;
             }
@@ -173,56 +168,11 @@ const ProjectTaskTree = () => {
     }
   };
 
-  const handleOpenDialog = (ref = null, parent_id) => {
-    setNewTask((prev) => ({ ...prev, parent_task_id: parent_id }));
+  const handleOpenDialog = (ref = null, parent_id, order) => {
+    setNewTask((prev) => ({ ...prev, parent_task_id: parent_id, order }));
     setEditingTask(false);
     setOpen(true);
     setOpenTaskRef(ref);
-  };
-
-  const findTree = (nodeId) => {
-    // given nodeId, find the tree element that is the node. this includes all the subtask elements
-    // will be recursive
-
-    console.log("renderedTasks", renderedTasks);
-
-    for (const task of renderedTasks) {
-      console.log("task", task);
-    }
-
-    const node = taskDetails[nodeId];
-    if (!node) return null;
-
-    const recursiveTreeSearch = (nodeId, dict, parentTree) => {
-      console.log("nodeId", nodeId);
-      console.log("dict", dict);
-      console.log("parentTree", parentTree);
-
-      if (nodeId === parentTree.key) {
-        return parentTree;
-      }
-
-      for (const [subtaskId, subtasks] of Object.entries(dict)) {
-        for (const child of parentTree.props.children) {
-          if (child.key === subtaskId) {
-            return child;
-          }
-          if (recursiveTreeSearch(nodeId, subtasks, child)) {
-            return parentTree.children;
-          }
-        }
-      }
-
-      return null;
-    };
-
-    // return recursiveTreeSearch(nodeId, subtasksDict, renderedTasks);
-    // for (const task of renderedTasks) {
-    //   if (recursiveTreeSearch(nodeId, subtasksDict, task)) {
-    //     return task;
-    //   }
-    // }
-
   };
 
   const handleOpenEditDialog = (ref, taskId) => {
@@ -240,9 +190,104 @@ const ProjectTaskTree = () => {
   };
 
   const handleCreateTask = async () => {
-    try {
-      const task = await createTask(newTask);
+    await handleTaskAction(createTask, "Error creating task.");
+  };
+
+  const handleEditTask = async () => {
+    await handleTaskAction(updateTask, "Error editing task.");
+  };
+
+  const handleDeleteTask = async (ref, taskId) => {
+    await handleTaskAction(deleteTask, "Error deleting task.", taskId);
+  };
+
+  const handleMoveTask = async (ref, taskId, direction) => {
+    const findSiblings = (taskId, dict) => {
+      if (dict[taskId]) {
+        return Object.keys(dict);
+      } else {
+        for (const [parentId, subtasks] of Object.entries(dict)) {
+          const result = findSiblings(taskId, subtasks);
+          if (result) {
+            return result;
+          }
+        }
+      }
+      return null;
+    };
+
+    const task = taskDetails[taskId];
+    const siblingTasks = task.parent_task_id === null ? Object.keys(subtasksDict) : (findSiblings(task.id, subtasksDict) || []);
+
+    const currentOrder = task.order;
+    const newOrder = currentOrder + direction;
+    const siblingCount = Object.keys(siblingTasks).length;
+    // const siblingTaskId = Object.keys(siblingTasks).find(key => taskDetails[key].order === newOrder);
+
+    const siblingTaskId = siblingTasks.find(key => {
+      const details = taskDetails[key];
+      // console.log("details", details);
+      const order = details.order;
+      // console.log("order", order);
+      const bool = order === newOrder;
+      // console.log("bool", bool);
+      return bool;
+    });
+
+    console.log("task", task);
+    console.log("siblingTasks", siblingTasks);
+    console.log("currentOrder", currentOrder);
+    console.log("newOrder", newOrder);
+    console.log("siblingCount", siblingCount);
+    console.log("siblingTaskId", siblingTaskId);
+
+    if (newOrder < 0 || newOrder > (siblingCount - 1)) return;
+
+    if (siblingTaskId) {
+      taskDetails[siblingTaskId].order = currentOrder;
+      taskDetails[taskId].order = newOrder;
+
+      await updateTask(taskDetails[siblingTaskId]);
+      await updateTask(taskDetails[taskId]);
+
       await loadProjectDetails();
+
+      if (ref && ref.current) {
+        const root = createRoot(ref.current);
+        const parentTree = findTree(task.parent_task_id);
+        if (parentTree) {
+          const updatedTree = renderTree(task.parent_task_id, findSiblings(task.parent_task_id, subtasksDict));
+          root.render(updatedTree);
+        }
+      }
+    }
+  };
+
+  const handleDeleteProject = async (projectId) => {
+    await deleteProject(projectId);
+    navigate("/dashboard/projects");
+  };
+
+  const handleTaskChange = (e) => {
+    const { name, value } = e.target;
+    setNewTask((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleExpandedItemsChange = (event, itemIds) => {
+    setExpandedItems(itemIds);
+  };
+
+  const handleTaskAction = async (action, errorMessage, taskId = null) => {
+    try {
+      const task = taskId ? await action(taskId) : await action(newTask);
+      await loadProjectDetails();
+
+      if (taskId) {
+        taskDetails[task.id] = task;
+      }
 
       const findChildren = (nodeId, dict) => {
         if (dict[nodeId]) {
@@ -268,94 +313,52 @@ const ProjectTaskTree = () => {
       setExpandedItems((prev) => [...new Set([...prev, newTask.parent_task_id, task.id])]);
 
       handleCloseDialog();
+    } catch (error) {
+      console.error(errorMessage, error);
+      setError(errorMessage);
     }
-    catch (error) {
-      console.error("Error creating task:", error);
-      setError("Error creating task.");
-    }
-  }
+  };
 
-  const handleEditTask = async () => {
-    try {
-      const task = await updateTask(newTask);
+  const findTree = (nodeId) => {
+    for (const task of renderedTasks) {
       console.log("task", task);
+    }
 
-      taskDetails[task.id] = task;
+    const node = taskDetails[nodeId];
+    if (!node) return null;
 
-      const findChildren = (nodeId, dict) => {
-        if (dict[nodeId]) {
-          return dict[nodeId];
-        } else {
-          for (const [parentId, subtasks] of Object.entries(dict)) {
-            if (findChildren(nodeId, subtasks)) {
-              return subtasks;
-            }
+    const recursiveTreeSearch = (nodeId, dict, parentTree) => {
+      if (nodeId === parentTree.key) {
+        return parentTree;
+      }
+
+      for (const [subtaskId, subtasks] of Object.entries(dict)) {
+        for (const child of parentTree.props.children) {
+          if (child.key === subtaskId) {
+            return child;
+          }
+          if (recursiveTreeSearch(nodeId, subtasks, child)) {
+            return parentTree.children;
           }
         }
-      };
-      
-      const newTree = (
-        <SimpleTreeView>
-          {renderTree(task.id, findChildren(task.id, subtasksDict))}
-        </SimpleTreeView>
-      );
-
-      if (openTaskRef && openTaskRef.current) {
-        const root = createRoot(openTaskRef.current);
-        root.render(newTree);
       }
 
-      handleCloseDialog();
-    }
-    catch (error) {
-      console.error("Error editing task:", error);
-      setError("Error editing task.");
-    }
-  }
-
-  const handleDeleteTask = async (ref, taskId) => {
-    try {
-      const details = taskDetails[taskId];
-      
-      await deleteTask(taskId);
-      await loadProjectDetails();
-
-      const parentTree = findTree(details.parent_task_id);
-      if (parentTree) {
-        const root = createRoot(parentTree.ref.current);
-        root.render(renderTree(details.parent_task_id, subtasksDict));
-      }
-    } catch (error) {
-      console.error("Error deleting task:", error);
-      setError("Error deleting task.");
-    }
-  };
-
-  const handleDeleteProject = async (projectId) => {
-    await deleteProject(projectId);
-    navigate("/dashboard/projects");
-  };
-
-  const handleTaskChange = (e) => {
-    const { name, value } = e.target;
-    setNewTask((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const handleExpandedItemsChange = (event, itemIds) => {
-    setExpandedItems(itemIds);
+      return null;
+    };
   };
 
   const renderTree = (nodeId, dict) => {
+    console.log("dict", dict);
     const node = taskDetails[nodeId];
+    console.log("node", node);
     if (!node) return null;
 
     const assignedAccount = accounts.find(emp => emp.id === node.assigned_to) || { name: "Unassigned" };
     const formattedDate = new Date(node.task_created).toLocaleDateString();
 
     const taskRef = React.createRef();
+
+
     return (
       <TreeItem2 key={node.id} itemId={node.id} ref={taskRef} label={
         <div className="task-node">
@@ -370,12 +373,20 @@ const ProjectTaskTree = () => {
               <Button className="settings-button" onClick={(e) => { e.stopPropagation(); handleOpenEditDialog(taskRef, node.id); }} startIcon={<SettingsIcon />}>
                 Edit
               </Button>
-              <Button className="add-button" onClick={(e) => { e.stopPropagation(); handleOpenDialog(taskRef, node.id); }}>
+              <Button className="add-button" onClick={(e) => { e.stopPropagation(); handleOpenDialog(taskRef, node.id, Object.keys(dict[node.id]).length); }}>
                 <FontAwesomeIcon icon={faPlus} />
               </Button>
               <Button className="delete-button" onClick={(e) => { e.stopPropagation(); handleDeleteTask(taskRef, node.id); }}>
                 <FontAwesomeIcon icon={faTrash} />
               </Button>
+              <div className="order-buttons">
+                <Button className="arrow-icon" onClick={(e) => { e.stopPropagation(); handleMoveTask(taskRef, node.id, -1); }}>
+                  <FontAwesomeIcon icon={faArrowUp} />
+                </Button>
+                <Button className="arrow-icon" onClick={(e) => { e.stopPropagation(); handleMoveTask(taskRef, node.id, 1); }}>
+                  <FontAwesomeIcon icon={faArrowDown} />
+                </Button>
+              </div>
             </div>
           </div>
         </div>
@@ -386,7 +397,6 @@ const ProjectTaskTree = () => {
           },
         }}
       >
-        {/* {console.log("taskRef", taskRef)} */}
         {dict[node.id] &&
           Object.keys(dict[node.id]).map((subtaskId) => renderTree(subtaskId, dict[node.id]))}
       </TreeItem2>
@@ -431,10 +441,13 @@ const ProjectTaskTree = () => {
             },
           }}
         >
-          {/* {console.log("prev", Object.keys(project.tasks).map((subtaskId) => renderTree(subtaskId, project.tasks)))} */}
           {renderedTasks}
           <div className="project-btn-container">
-            <Button className="add-button" onClick={() => handleOpenDialog(null)} startIcon={<FontAwesomeIcon icon={faPlus} />} >Add Root Task</Button>
+            <Button className="add-button" onClick={() => {
+              handleOpenDialog(null, null, rootTaskOrder);
+              setRootTaskOrder(rootTaskOrder + 1);
+            }} startIcon={<FontAwesomeIcon icon={faPlus} />} >Add Root Task</Button>
+
             <Button className="delete-button" onClick={() => handleDeleteProject(project_id)} startIcon={<FontAwesomeIcon icon={faTrash} />}>
               Delete Project</Button>
           </div>
